a916b9d8342cb6628db8d807241ce55d1265d289
Kallesh B S <kalleshbs47@gmail.com>
2025-11-11 17:29:27 +0530

    scripts updated

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use("/api", digitalEntertainmentRoutes);
 app.use(generalErrorHandler)
 const port = process.env.PORT || 4112
-app.listen(4112, () => {
+app.listen(port, () => {
     console.log(`Server running on ${port}`);
 })
